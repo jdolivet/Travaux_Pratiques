67cb73aa25f3e7aa67036089ece6ed8bd97071eb
Johann DOLIVET <jdolivet@hotmail.com>
2018-12-04 20:57:37 -0200

    Cleaning

--- scripts/.ipynb_checkpoints/bouton-checkpoint.js
+++
@@ -1,7 +0,0 @@
-function masquemod(id){
-			   if(document.getElementById(id).style.display=='none'){
-			      document.getElementById(id).style.display='block';
-			   }else{
-			      document.getElementById(id).style.display='none';
-			   }
-			}
